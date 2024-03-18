d4929a7745dbf690675fa39d4d158f3dab36b99b
M.Ibad khan <160883583+IbadKhan213@users.noreply.github.com>
2024-03-19 00:02:12 +0500

    increment or decrement

---
+++ main.ts
@@ -0,0 +1,71 @@
+// INCREMENT & DECREMENT OPERATORS
+
+//1.INCREMENT:(++)
+//It is same like adition but it only add 1 in a variable everytime
+//There are two types,
+
+//1.Pre-Increment:(++a)
+// Pre-Increment is to increase the numerical count of the variable by a value of 1.
+/*
+let a = 10;
+
+console.log(++a);   //here answer will be 11.
+console.log(++a);   //here answer will be 12.
+*/
+
+//2.Post-Increment:(a++)
+//In Post-Decrement,the value of the variable is first used in an expression and then increase by a value of 1.
+/*
+let b = 20;
+
+console.log(b++);   //also here answer will be 20.
+console.log(b++);   //Now, answer will be 21.
+*/
+   
+
+//2.DECREMENT:(--)
+//It is same like adition but it only sub 1 in a variable everytime
+//There are two types,
+
+//1.Pre-Decrement:(--c)
+// Pre-Increment is to decrease the numerical count of the variable by a value of 1.
+/*
+let c = 30;
+
+console.log(--c);   //here answer will be 29.
+console.log(--c);   //here answer will be 28.
+*/
+
+//2.Post-Decrement:(d--)
+//In Post-Decrement,the value of the variable is first used in an expression and then increase by a value of 1.
+/*
+let d = 40;
+
+console.log(d--);   //also here answer will be 40.
+console.log(d--);   //Now, answer will be 39.
+*/
+
+
+
+//HOMEWORK
+
+/*
+Q:01 
+
+let x = 20;
+
+ console.log((++x)+(x++)+ (x));
+*/
+
+/*
+Q:02
+
+let y = 50;
+let num1 =  y++;
+let num2 =  --y;
+let num3 = ++y ;
+
+console.log(num1 + num2 - num3);
+*/
+
+
